24dfaf0cf1bbfdedfb834245dce9c6c7953320e0
Yusuf <josef198827@gmail.com>
2023-08-13 14:24:33 -0500

    .env file location changed and a small change

--- utils/database.js
+++ utils/database.js
@@ -13,14 +13,11 @@ export const connectToDB = async () => {
   // that is why I placed my mongodb string manually.
 
   try {
-    await mongoose.connect(
-      "mongodb+srv://yusuffirat:Cluster0.123@cluster0.piybins.mongodb.net/?retryWrites=true&w=majority",
-      {
-        dbName: "share_prompt",
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
