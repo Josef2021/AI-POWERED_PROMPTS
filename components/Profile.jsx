daa419f894eeb80d43ea2f78977d11ac8508d252
Yusuf <josef198827@gmail.com>
2023-08-26 21:08:22 -0500

    edit and delete prompt is applied.

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,20 +1,20 @@
-import React from "react";
 import PromptCard from "./PromptCard";
 
 const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
-    <section className="w-full text-left;">
-      <h1 className="head_text text-left ">
+    <section className="w-full">
+      <h1 className="head_text text-left">
         <span className="blue_gradient">{name} Profile</span>
       </h1>
       <p className="desc text-left">{desc}</p>
+
       <div className="mt-10 prompt_layout">
         {data.map((post) => (
           <PromptCard
             key={post._id}
             post={post}
-            handleEdit={handleEdit && handleEdit(post)}
-            handleDelete={handleDelete && handleDelete(post)}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
           />
         ))}
       </div>
